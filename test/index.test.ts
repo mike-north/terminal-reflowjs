b8df39980161e05e3fa6361525a5e60b4bb729a6
Mike North <michael.l.north@gmail.com>
2025-12-09 07:38:06 -0800

    initial commit

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { helloWorld } from "@";
+
+describe("helloWorld", () => {
+  it("should return 'Hello World'", () => {
+    expect(helloWorld()).toBe("Hello World");
+  });
+});
